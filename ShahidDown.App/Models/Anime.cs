34b5974837b6cd825e7e13ad47ebbce5300fec3e
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-04-23 13:28:56 +0100

    chore: Add Anime class with AnimeType enum in /Models

---
+++ ShahidDown.App/Models/Anime.cs
@@ -0,0 +1,24 @@
+﻿namespace ShahidDown.App.Models
+{
+    /// <summary>
+    /// Represents the type of an anime.
+    /// </summary>
+    public enum AnimeType
+    {
+        All,
+        Tv,
+        Movie,
+        Ona
+    }
+
+    /// <summary>
+    /// Represents an anime with its details.
+    /// </summary>
+    public class Anime
+    {
+        public int Id { get; set; }
+        public required string Title { get; set; }
+        public AnimeType Type { get; set; }
+        public int Episodes { get; set; } 
+    }
+}
