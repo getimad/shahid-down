687e1794e9f45e0ed195834c0af99e9890f9b27e
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-04-24 20:30:02 +0100

    chore: Move MainWindow.xaml to /Views

--- ShahidDown.App/Views/MainWindow.xaml.cs
+++ ShahidDown.App/Views/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
-﻿using System.Text;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -6,10 +10,9 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ShahidDown.App
+namespace ShahidDown.App.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
