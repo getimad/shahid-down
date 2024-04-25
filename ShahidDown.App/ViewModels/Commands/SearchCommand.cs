7f2ecec4b002cc1c82c88e9a9b36e0701136ef80
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-04-25 02:24:14 +0100

    feat: Add SearchCommand class for search button

---
+++ ShahidDown.App/ViewModels/Commands/SearchCommand.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows.Input;
+
+namespace ShahidDown.App.ViewModels.Commands
+{
+    public class SearchCommand : ICommand
+    {
+        public AnimeVM AnimeVM { get; set; }
+
+        public event EventHandler? CanExecuteChanged;
+
+        public SearchCommand(AnimeVM animeVM)
+        {
+            AnimeVM = animeVM;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return true; // Always return true
+        }
+
+        public void Execute(object? parameter)
+        {
+            AnimeVM.SearchAnime();
+        }
+    }
+}
