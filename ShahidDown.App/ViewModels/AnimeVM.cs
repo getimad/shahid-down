faad44bdb8b52e614cc86895407a37faa175234c
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-04-25 02:23:37 +0100

    feat: Add AnimeVM for MainWindow
    - Add SearchQuery, AnimeList and SeachCommand props to AnimeVM.
    - Bind all props of AnimeVM to MainWindow View.

---
+++ ShahidDown.App/ViewModels/AnimeVM.cs
@@ -0,0 +1,59 @@
+﻿using ShahidDown.App.Models;
+using ShahidDown.App.ViewModels.Commands;
+using ShahidDown.App.ViewModels.Helpers;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+
+namespace ShahidDown.App.ViewModels
+{
+    public class AnimeVM : INotifyPropertyChanged
+    {
+        private string? _searchQuery;
+
+        public string? SearchQuery
+        {
+            get => _searchQuery;
+            set
+            {
+                _searchQuery = value;
+                OnPropertyChanged(nameof(SearchQuery));
+            }
+        }
+
+        private ObservableCollection<Anime>? _animeList;
+
+        public ObservableCollection<Anime>? AnimeList
+        {
+            get => _animeList;
+            set
+            {
+                _animeList = value;
+                OnPropertyChanged(nameof(AnimeList));
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public AnimeVM()
+        {
+            SearchCommand = new SearchCommand(this);
+        }
+
+        public void SearchAnime()
+        {
+            if (string.IsNullOrEmpty(SearchQuery))
+            {
+                return;
+            }
+
+            AnimeList = new ObservableCollection<Anime>(Scraper.ScrapAnimeList(SearchQuery));
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        private void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
