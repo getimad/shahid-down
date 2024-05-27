85d07c8103f181aa2043965361fb8478cbc3a475
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-05-27 20:57:22 +0100

    feat: Add double click event to open AnimeItemWindow

--- ShahidDown.App/ViewModels/AnimeListVM.cs
+++ ShahidDown.App/ViewModels/AnimeListVM.cs
@@ -1,7 +1,7 @@
 ﻿using ShahidDown.App.Models;
-using ShahidDown.App.Services;
 using ShahidDown.App.ViewModels.Commands;
 using ShahidDown.App.ViewModels.Helpers;
+using ShahidDown.App.Views;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Windows.Input;
@@ -35,11 +35,14 @@ namespace ShahidDown.App.ViewModels
 
         public ICommand SelectedCommand { get; }
 
+        public ICommand OpenAnimeItemWindowCommand { get; }
+
         public AnimeListVM()
         {
             Messenger.Instance.Register(nameof(OnSearchCommandExecuted), OnSearchCommandExecuted);
 
             SelectedCommand = new RelayCommand(param => OnItemSelectedCommandExecuted(), param => CanItemSelectedCommandExecute());
+            OpenAnimeItemWindowCommand = new RelayCommand(param => OnOpenAnimeItemWindowCommandExecuted(), param => CanOpenAnimeItemWindowCommandExecute());
         }
 
         private void OnSearchCommandExecuted(object data)
@@ -53,6 +56,17 @@ namespace ShahidDown.App.ViewModels
         }
 
         private bool CanItemSelectedCommandExecute()
+        {
+            return SelectedAnime != null;
+        }
+
+        private void OnOpenAnimeItemWindowCommandExecuted()
+        {
+            AnimeItemWindow animeItemWindow = new AnimeItemWindow();
+            animeItemWindow.ShowDialog();
+        }
+
+        private bool CanOpenAnimeItemWindowCommandExecute()
         {
             return SelectedAnime != null;
         }
