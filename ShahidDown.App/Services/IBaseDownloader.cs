dc1be3de344d5e57fa0f52a0e2cf6ceaa0d2d7d7
Imad Ez-zahi <107067409+getimad@users.noreply.github.com>
2024-05-31 21:35:42 +0100

    refactor: Convert BaseDownloader class to an interface

---
+++ ShahidDown.App/Services/IBaseDownloader.cs
@@ -0,0 +1,24 @@
+﻿using OpenQA.Selenium;
+
+namespace ShahidDown.App.Services
+{
+    /// <summary>
+    /// This interface is the base downloader.
+    /// </summary>
+    public interface IBaseDownloader
+    {
+        public string DownloadPath { get; }
+
+        public IWebDriver DownloadDriver { get; }
+
+        public void Start(string url);
+
+        public bool CanStart(string url);
+
+        public void Stop();
+
+        public void WaitForFileDownload(string title);
+
+        public bool IsFileExist(string title);
+    }
+}
